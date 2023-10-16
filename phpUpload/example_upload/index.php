f773cb666e224ead83dc49c84ad59ef01673746e
asj <47907504+sjahn94@users.noreply.github.com>
2023-10-16 21:24:11 +0900

    Create index.php

---
+++ phpUpload/example_upload/index.php
@@ -0,0 +1,8 @@
+<?php
+        header("Content-Type: text/html; charset=UTF-8");
+?>
+
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+<input type="file" name="userfile">
+<input type="submit" value="UPLOAD">
+</form>
